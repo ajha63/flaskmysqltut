0bb89afea58815cc4ca1265c4b0e404f409b27c5
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-08-16 15:26:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flaskmysqltut
+
+=A Flask MySQL CRUD based on Mbithe Nzomo (@mbithenzomo) https://scotch.io Tutorial
---
+++ app/auth/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# app/auth/__init__.py
+
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
