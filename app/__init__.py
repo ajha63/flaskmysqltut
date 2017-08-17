22d93020fe5bcadd90f45b774f9c045bcd5c5fe2
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-08-17 10:09:31 -0400

    First app refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@
 
 # third-party imports
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
 
-# after existing third-party imports
-from flask_login import LoginManager
-from flask_migrate import Migrate
 
 # db variable initialization
 db = SQLAlchemy()
@@ -24,17 +24,16 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-
     migrate = Migrate(app, db)
 
     from app import models
 
-    # Blueprints
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
@@ -45,3 +44,16 @@ def create_app(config_name):
     app.register_blueprint(home_blueprint)
 
     return app
+    #from app import models
+
+    # Blueprints
+    #from .admin import admin as admin_blueprint
+    #app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    #from .auth import auth as auth_blueprint
+    #app.register_blueprint(auth_blueprint)
+
+    #from .home import home as home_blueprint
+    #app.register_blueprint(home_blueprint)
+
+    #return app
